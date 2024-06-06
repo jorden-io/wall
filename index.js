ef65437330e04144697005d1fdcfe2a26746eef0
JorbyWasTaken <jordenrodriguez1004@yahoo.com>
2024-06-06 08:18:05 -0500

    changed port to 8000

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(5001, () => "api running on 5001");
+app.listen(8000, () => "api running on 8000");
 
 app.get("/", (req, res) => res.json("running"));
