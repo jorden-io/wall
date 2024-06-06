5cb5d05b0224a10cfa58235c3de93f353b0c7192
JorbyWasTaken <jordenrodriguez1004@yahoo.com>
2024-06-06 08:25:17 -0500

    added log

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(8000, () => "api running on 8000");
+app.listen(8000, () => console.log("api running on 8000"));
 
 app.get("/", (req, res) => res.json("running"));
