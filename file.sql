e038696c797f7558f86094057b7bbcb6c2b9d477
Jorden Rodriguez <jorden@Jordens-Mac-mini.local>
2024-06-06 11:03:04 -0500

    added db

---
+++ file.sql
@@ -0,0 +1,4 @@
+CREATE TABLE messages (
+  mid SERIAL PRIMARY KEY,
+  data VARCHAR(255)
+);
--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 import express from "express";
-
 const app = express();
+import pg from "pg";
+import cors from "cors";
+
+//DB CONNECTION
+const pool = new pg.Client({
+  user: "postgres",
+  password: "postgres",
+  host: "pgdb.ch608saq4znm.us-east-2.rds.amazonaws.com",
+  port: 5431,
+  database: "postgres",
+  //ssl: true,
+});
+await pool.connect();
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.listen(8000, () => console.log("api running on 8000"));
 
-app.get("/", (req, res) => res.json("running"));
+//app.get("/", (req, res) => res.json("works"));
+app.get("/", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM messages");
+    res.json(data.rows[0]);
+    console.log(res.json(data.rows));
+  } catch {}
+});
+app.get("/", async (req, res) => {
+  try {
+    res.json("hi");
+  } catch {}
+});
